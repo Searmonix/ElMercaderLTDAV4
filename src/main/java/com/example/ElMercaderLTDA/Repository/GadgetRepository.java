fed0cf88101bf84198fdfdef7c35add3c5d5f2ba
searmonix@gmail.com <diego.hernandez2.at@usa.edu.co>
2023-08-11 19:21:29 -0500

    [Upload]: Dockerization and Frontend
    React + Vite and Docker

--- src/main/java/com/example/ElMercaderLTDA/Repository/GadgetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ElMercaderLTDA.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.ElMercaderLTDA.Model.Gadget;
-
-public interface GadgetRepository extends MongoRepository<Gadget, Integer>{
-    
-}
