3fe1680538d44086d71e9cc4ff1f53c24e311e67
searmonix@gmail.com <diego.hernandez2.at@usa.edu.co>
2023-08-14 23:08:51 -0500

    [Upload]: Base User Added
    For frontend usage

--- ElMercader_mongo/mongo-init.js
+++ ElMercader_mongo/mongo-init.js
@@ -12,3 +12,16 @@ db.createUser({
 db.createCollection('usuarios');
 db.createCollection('orders');
 db.createCollection('gadgets');
+
+db.usuarios.insertOne({
+    _id: 1,
+    identification: '1000',
+    name: 'First Admin',
+    address: 'Crr 127 Cll 0 #0-1',
+    cellPhone: '',
+    email: 'usaeste@correo.com',
+    password: '1234',
+    zone: 'Zona 1',
+    type: 'ADM',
+    _class: 'com.example.ElMercaderLTDA.Model.User'
+});
